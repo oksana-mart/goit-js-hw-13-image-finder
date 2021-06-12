34df30e8be53b2763e1b7c725e116b43852938bc
Oksana Martynenko <oks.martynenko@gmail.com>
2021-06-12 21:36:52 +0300

    Добавляет ApiService

---
+++ src/js/apiService.js
@@ -0,0 +1,42 @@
+const API_KEY = '22042926-beb76aab4ca671d50b680dadc';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    //console.log(this);
+    const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&per_page=12&page=${this.page}`;
+    return fetch(url)
+    .then(responce => responce.json())
+    .then(({hits}) => { 
+      //console.log({hits});
+      this.incrementPage();
+      if (hits.length === 0) {
+        return alert('Try to enter something again!');
+      }
+      return hits;
+    });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query (newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+
+
